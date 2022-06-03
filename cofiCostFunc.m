b0615bd7becf986a4133de68e326212f499f1625
arjuns238 <92126319+arjuns238@users.noreply.github.com>
2022-06-03 13:48:35 -0400

    Update cofiCostFunc.m

--- cofiCostFunc.m
+++ cofiCostFunc.m
@@ -12,28 +12,18 @@ Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
             
-% You need to return the following values correctly
+% Initializing the variable we need to return
 J = 0;
 X_grad = zeros(size(X));
 Theta_grad = zeros(size(Theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost function and gradient for collaborative
-%               filtering. Concretely, you should first implement the cost
-%               function (without regularization) and make sure it
-%               matches our costs. After that, you should implement the 
-%               gradient and use the checkCostFunction routine to check
-%               that the gradient is correct. Finally, you should implement
-%               regularization.
-%
+% ======================  CODE  ======================
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
 %        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
 %            i-th movie was rated by the j-th user
-%
-% You should set the following variables correctly:
-%
+%%
 %        X_grad - num_movies x num_features matrix, containing the 
 %                 partial derivatives w.r.t. to each element of X
 %        Theta_grad - num_users x num_features matrix, containing the 
